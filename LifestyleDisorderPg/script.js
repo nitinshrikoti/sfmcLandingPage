d466ea7aee412761c3741526a646d16b7a59f85f
nitinshri <nitinshrikoti1@gmail.com>
2025-04-08 15:02:26 +0530

    new update

--- LifestyleDisorderPg/script.js
+++ LifestyleDisorderPg/script.js
@@ -46,6 +46,10 @@ const Testi3 =
   '"Great session! The explanations were clear, and I feel more confident about reversing my blood sugar levels & diabetes"';
 
 
+const TestiNam1 = "— Saheb";
+const TestiNam2 = "— Pryanka";
+const TestiNam3 = "— Sidharth";
+
 // Selectors for the dynamic content
 document.title = title;
 document.getElementById("heroHeading").innerHTML = heroHeading;
@@ -87,6 +91,10 @@ document.getElementById("ContactNumber").value = contactNumber;
 document.getElementById("testi1").innerHTML = Testi1;
 document.getElementById("testi2").innerHTML = Testi2;
 document.getElementById("testi3").innerHTML = Testi3;
+
+document.getElementById("testiNam1").innerHTML = TestiNam1;
+document.getElementById("testiNam2").innerHTML = TestiNam2;
+document.getElementById("testiNam3").innerHTML = TestiNam3;
 
 // Selectors for the dynamic content end
 
@@ -198,4 +206,33 @@ document.addEventListener("DOMContentLoaded", function () {
       menuToggle.checked = false;
     });
   });
+});
+
+// -------------------- Form country script --------------------
+const phoneInput = document.querySelector("#phone");
+const iti = window.intlTelInput(phoneInput, {
+  initialCountry: "in",
+  strictMode: true,
+  loadUtils: function () {
+    return import(
+      "https://cdn.jsdelivr.net/npm/intl-tel-input@25.3.1/build/js/utils.js"
+    );
+  },
+});
+
+const form = document.getElementById("RegisterForm");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // Stop form submission temporarily
+
+  const countryData = iti.getSelectedCountryData();
+
+  // Set hidden fields
+  document.getElementById("countryCode").value = countryData.dialCode;
+  document.getElementById("initialCountry").value = countryData.iso2;
+
+  // Now submit the form manually
+  setTimeout(() => {
+    form.submit();
+  }, 100); // Slight delay ensures fields are updated
 });
