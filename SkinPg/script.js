8bfb60c51dd9dc650e95107cf47a6e08972210da
nitinshri <nitinshrikoti1@gmail.com>
2025-04-08 13:40:26 +0530

    update

--- SkinPg/script.js
+++ SkinPg/script.js
@@ -49,6 +49,10 @@ const Testi2 =
 const Testi3 =
   '"Sheela Solki shares her experience of improving significantly after starting treatment at the institution, mentioning how her psoriasis, skin issues, and stomach pain were healed in just 3-4 months, and she now feels fully healthy. She thanks the doctors, especially Manish Sir, for giving her a new life and encourages others to try the treatment for better health."';
 
+const TestiNam1 = "— Mabel";
+const TestiNam2 = "— White Marks";
+const TestiNam3 = "— Shila";
+
 // Selectors for the dynamic content
 document.title = title;
 document.getElementById("heroHeading").innerHTML = heroHeading;
@@ -90,6 +94,10 @@ document.getElementById("ContactNumber").value = contactNumber;
 document.getElementById("testi1").innerHTML = Testi1;
 document.getElementById("testi2").innerHTML = Testi2;
 document.getElementById("testi3").innerHTML = Testi3;
+
+document.getElementById("testiNam1").innerHTML = TestiNam1;
+document.getElementById("testiNam2").innerHTML = TestiNam2;
+document.getElementById("testiNam3").innerHTML = TestiNam3;
 
 // Selectors for the dynamic content end
 
@@ -201,4 +209,33 @@ document.addEventListener("DOMContentLoaded", function () {
       menuToggle.checked = false;
     });
   });
+});
+
+// -------------------- Form country script --------------------
+const phoneInput = document.querySelector("#phone");
+const iti = window.intlTelInput(phoneInput, {
+  initialCountry: "in",
+  strictMode: true,
+  loadUtils: function () {
+    return import(
+      "https://cdn.jsdelivr.net/npm/intl-tel-input@25.3.1/build/js/utils.js"
+    );
+  },
+});
+
+const form = document.getElementById("RegisterForm");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // Stop form submission temporarily
+
+  const countryData = iti.getSelectedCountryData();
+
+  // Set hidden fields
+  document.getElementById("countryCode").value = countryData.dialCode;
+  document.getElementById("initialCountry").value = countryData.iso2;
+
+  // Now submit the form manually
+  setTimeout(() => {
+    form.submit();
+  }, 100); // Slight delay ensures fields are updated
 });
