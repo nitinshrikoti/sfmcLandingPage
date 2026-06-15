de23f3c88fe569a77c95926adc25f43ae7a499c8
nitinshri <nitinshrikoti1@gmail.com>
2026-06-15 16:56:19 +0530

    section 4 timer

---
+++ webinar v3/main.js
@@ -0,0 +1,35 @@
+const targetDate = new Date("2026-07-20T19:00:00").getTime();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+
+  const distance = targetDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = String(days).padStart(2, "0");
+
+  document.getElementById("hours").innerHTML = String(hours).padStart(2, "0");
+
+  document.getElementById("minutes").innerHTML = String(minutes).padStart(
+    2,
+    "0",
+  );
+
+  document.getElementById("seconds").innerHTML = String(seconds).padStart(
+    2,
+    "0",
+  );
+}
+
+setInterval(updateCountdown, 1000);
+
+updateCountdown();
