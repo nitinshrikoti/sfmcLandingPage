a6758fe9b7cac536535ab84ed3ed4df1274aa7ec
nitinshri <nitinshrikoti1@gmail.com>
2026-06-16 14:10:01 +0530

    section 10 faqs

--- webinar v3/main.js
+++ webinar v3/main.js
@@ -51,3 +51,19 @@ videoContainer.addEventListener("click", function () {
 
     `;
 });
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+  const button = item.querySelector(".faq-question");
+
+  button.addEventListener("click", () => {
+    faqItems.forEach((faq) => {
+      if (faq !== item) {
+        faq.classList.remove("active");
+      }
+    });
+
+    item.classList.toggle("active");
+  });
+});
